a60b0c3e4cb2f09e3b4bc9daa4fdc82992b82253
Ana Roza <analialarrozab@gmail.com>
2023-06-13 22:00:17 -0300

    cambios en varios archivos

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let nombreIngresado = promt ("Por favor, ingrese su nombre")
+let ciudadProveniente = promt ("Por favor, ingrese su ciudad")
+let espacio = " "
+let datosDeUsario = nombreIngresado + espacio + ciudadProveniente
+
+alert (datosDeUsario);
